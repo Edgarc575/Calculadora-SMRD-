d5f00b26cfe329938e73c6cb47fc52d52142e146
Ed <caceresrivera321@gmail.com>
2024-06-10 22:24:08 -0600

    Segundo commit
    -Se ha agregado el espacio del resultado.
    -Se ha agregado el codigo en la cual hace funcionar cada uno de los botones con su respectiva accion.

--- Calculadora_Sencilla_Csharp/Calculadora_Sencilla_Csharp/Form1.Designer.cs
+++ Calculadora_Sencilla_Csharp/Calculadora_Sencilla_Csharp/Form1.Designer.cs
@@ -30,14 +30,16 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lblprimernum = new System.Windows.Forms.Label();
-            this.lblsegundonum = new System.Windows.Forms.Label();
-            this.txtnum1 = new System.Windows.Forms.TextBox();
-            this.txtnum2 = new System.Windows.Forms.TextBox();
-            this.btnsuma = new System.Windows.Forms.Button();
-            this.btnresta = new System.Windows.Forms.Button();
-            this.btnmulti = new System.Windows.Forms.Button();
             this.btndiv = new System.Windows.Forms.Button();
+            this.btnmulti = new System.Windows.Forms.Button();
+            this.btnresta = new System.Windows.Forms.Button();
+            this.btnsuma = new System.Windows.Forms.Button();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.lblsegundonum = new System.Windows.Forms.Label();
+            this.lblprimernum = new System.Windows.Forms.Label();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.txtresultado = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,6 +56,8 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel1.Controls.Add(this.txtresultado);
+            this.panel1.Controls.Add(this.lblresultado);
             this.panel1.Controls.Add(this.btndiv);
             this.panel1.Controls.Add(this.btnmulti);
             this.panel1.Controls.Add(this.btnresta);
@@ -68,15 +72,61 @@
             this.panel1.Size = new System.Drawing.Size(794, 430);
             this.panel1.TabIndex = 1;
             // 
-            // lblprimernum
+            // btndiv
             // 
-            this.lblprimernum.AutoSize = true;
-            this.lblprimernum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblprimernum.Location = new System.Drawing.Point(50, 188);
-            this.lblprimernum.Name = "lblprimernum";
-            this.lblprimernum.Size = new System.Drawing.Size(158, 24);
-            this.lblprimernum.TabIndex = 1;
-            this.lblprimernum.Text = "Primer Numero:";
+            this.btndiv.Location = new System.Drawing.Point(610, 266);
+            this.btndiv.Name = "btndiv";
+            this.btndiv.Size = new System.Drawing.Size(112, 53);
+            this.btndiv.TabIndex = 8;
+            this.btndiv.Text = "Division";
+            this.btndiv.UseVisualStyleBackColor = true;
+            this.btndiv.Click += new System.EventHandler(this.btndiv_Click);
+            // 
+            // btnmulti
+            // 
+            this.btnmulti.Location = new System.Drawing.Point(465, 266);
+            this.btnmulti.Name = "btnmulti";
+            this.btnmulti.Size = new System.Drawing.Size(112, 53);
+            this.btnmulti.TabIndex = 7;
+            this.btnmulti.Text = "Multiplicacion";
+            this.btnmulti.UseVisualStyleBackColor = true;
+            this.btnmulti.Click += new System.EventHandler(this.btnmulti_Click);
+            // 
+            // btnresta
+            // 
+            this.btnresta.Location = new System.Drawing.Point(610, 171);
+            this.btnresta.Name = "btnresta";
+            this.btnresta.Size = new System.Drawing.Size(112, 53);
+            this.btnresta.TabIndex = 6;
+            this.btnresta.Text = "Resta";
+            this.btnresta.UseVisualStyleBackColor = true;
+            this.btnresta.Click += new System.EventHandler(this.btnresta_Click);
+            // 
+            // btnsuma
+            // 
+            this.btnsuma.Location = new System.Drawing.Point(465, 171);
+            this.btnsuma.Name = "btnsuma";
+            this.btnsuma.Size = new System.Drawing.Size(112, 53);
+            this.btnsuma.TabIndex = 5;
+            this.btnsuma.Text = "Suma";
+            this.btnsuma.UseVisualStyleBackColor = true;
+            this.btnsuma.Click += new System.EventHandler(this.btnsuma_Click);
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(258, 295);
+            this.txtnum2.Multiline = true;
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(135, 24);
+            this.txtnum2.TabIndex = 4;
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Location = new System.Drawing.Point(258, 188);
+            this.txtnum1.Multiline = true;
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(135, 24);
+            this.txtnum1.TabIndex = 3;
             // 
             // lblsegundonum
             // 
@@ -88,57 +138,34 @@
             this.lblsegundonum.TabIndex = 2;
             this.lblsegundonum.Text = "Segundo Numero:";
             // 
-            // txtnum1
+            // lblprimernum
             // 
-            this.txtnum1.Location = new System.Drawing.Point(258, 188);
-            this.txtnum1.Multiline = true;
-            this.txtnum1.Name = "txtnum1";
-            this.txtnum1.Size = new System.Drawing.Size(135, 24);
-            this.txtnum1.TabIndex = 3;
+            this.lblprimernum.AutoSize = true;
+            this.lblprimernum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblprimernum.Location = new System.Drawing.Point(50, 188);
+            this.lblprimernum.Name = "lblprimernum";
+            this.lblprimernum.Size = new System.Drawing.Size(158, 24);
+            this.lblprimernum.TabIndex = 1;
+            this.lblprimernum.Text = "Primer Numero:";
             // 
-            // txtnum2
+            // lblresultado
             // 
-            this.txtnum2.Location = new System.Drawing.Point(258, 295);
-            this.txtnum2.Multiline = true;
-            this.txtnum2.Name = "txtnum2";
-            this.txtnum2.Size = new System.Drawing.Size(135, 24);
-            this.txtnum2.TabIndex = 4;
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(202, 367);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(109, 24);
+            this.lblresultado.TabIndex = 9;
+            this.lblresultado.Text = "Resultado:";
             // 
-            // btnsuma
+            // txtresultado
             // 
-            this.btnsuma.Location = new System.Drawing.Point(465, 171);
-            this.btnsuma.Name = "btnsuma";
-            this.btnsuma.Size = new System.Drawing.Size(112, 53);
-            this.btnsuma.TabIndex = 5;
-            this.btnsuma.Text = "Suma";
-            this.btnsuma.UseVisualStyleBackColor = true;
-            // 
-            // btnresta
-            // 
-            this.btnresta.Location = new System.Drawing.Point(610, 171);
-            this.btnresta.Name = "btnresta";
-            this.btnresta.Size = new System.Drawing.Size(112, 53);
-            this.btnresta.TabIndex = 6;
-            this.btnresta.Text = "Resta";
-            this.btnresta.UseVisualStyleBackColor = true;
-            // 
-            // btnmulti
-            // 
-            this.btnmulti.Location = new System.Drawing.Point(465, 266);
-            this.btnmulti.Name = "btnmulti";
-            this.btnmulti.Size = new System.Drawing.Size(112, 53);
-            this.btnmulti.TabIndex = 7;
-            this.btnmulti.Text = "Multiplicacion";
-            this.btnmulti.UseVisualStyleBackColor = true;
-            // 
-            // btndiv
-            // 
-            this.btndiv.Location = new System.Drawing.Point(610, 266);
-            this.btndiv.Name = "btndiv";
-            this.btndiv.Size = new System.Drawing.Size(112, 53);
-            this.btndiv.TabIndex = 8;
-            this.btndiv.Text = "Division";
-            this.btndiv.UseVisualStyleBackColor = true;
+            this.txtresultado.Enabled = false;
+            this.txtresultado.Location = new System.Drawing.Point(339, 367);
+            this.txtresultado.Multiline = true;
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.Size = new System.Drawing.Size(135, 24);
+            this.txtresultado.TabIndex = 10;
             // 
             // Form1
             // 
@@ -148,7 +175,9 @@
             this.ClientSize = new System.Drawing.Size(818, 454);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -167,6 +196,8 @@
         private System.Windows.Forms.TextBox txtnum1;
         private System.Windows.Forms.Label lblsegundonum;
         private System.Windows.Forms.Label lblprimernum;
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.Label lblresultado;
     }
 }
 
--- Calculadora_Sencilla_Csharp/Calculadora_Sencilla_Csharp/Form1.cs
+++ Calculadora_Sencilla_Csharp/Calculadora_Sencilla_Csharp/Form1.cs
@@ -15,6 +15,83 @@ namespace Calculadora_Sencilla_Csharp
         public Form1()
         {
             InitializeComponent();
+           
+        }
+
+        //Se crea una clase publica para las variables globales
+        public static class VGlobal
+        {
+            public static double num1; //Numero 1 tipo doble 
+            public static double num2; //Numero 2 tipo doble
+            public static double resultado=0; //Resultado tipo doble
+            
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Boton suma
+        private void btnsuma_Click(object sender, EventArgs e)
+        {
+            //Se almacena en las variables lo que contiene la caja de texto
+            VGlobal.num1 = Convert.ToDouble(txtnum1.Text);
+            VGlobal.num2 = Convert.ToDouble(txtnum2.Text);
+
+            //Proceso de suma
+            VGlobal.resultado = VGlobal.num1 + VGlobal.num2;
+
+            //El resultado se mostrara en la caja de texto
+            txtresultado.Text = Convert.ToString(VGlobal.resultado);
+
+        }
+
+        //Boton resta
+        private void btnresta_Click(object sender, EventArgs e)
+        {
+            //Se almacena en las variables lo que contiene la caja de texto
+            VGlobal.num1 = Convert.ToDouble(txtnum1.Text);
+            VGlobal.num2 = Convert.ToDouble(txtnum2.Text);
+
+            //Proceso de resta
+            VGlobal.resultado = VGlobal.num1 - VGlobal.num2;
+
+            //El resultado se mostrara en la caja de texto
+            txtresultado.Text = Convert.ToString(VGlobal.resultado);
+
+        }
+
+        //Boton multiplicacion
+        private void btnmulti_Click(object sender, EventArgs e)
+        {
+            //Se almacena en las variables lo que contiene la caja de texto
+            VGlobal.num1 = Convert.ToDouble(txtnum1.Text);
+            VGlobal.num2 = Convert.ToDouble(txtnum2.Text);
+
+            //Proceso de multiplicacion
+            VGlobal.resultado = VGlobal.num1 * VGlobal.num2;
+
+            //El resultado se mostrara en la caja de texto
+            txtresultado.Text = Convert.ToString(VGlobal.resultado);
+
+        }
+
+        //Boton division
+        private void btndiv_Click(object sender, EventArgs e)
+        {
+            //Se almacena en las variables lo que contiene la caja de texto
+            VGlobal.num1 = Convert.ToDouble(txtnum1.Text);
+            VGlobal.num2 = Convert.ToDouble(txtnum2.Text);
+
+            //Proceso de division
+            VGlobal.resultado = VGlobal.num1 / VGlobal.num2;
+
+            //El resultado se mostrara en la caja de texto
+            txtresultado.Text = Convert.ToString(Math.Round(VGlobal.resultado,2));//Solo 2 numeros despues del punto
+
         }
     }
 }
